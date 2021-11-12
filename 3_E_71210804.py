02a571cf47cec3294bc7614d52110cb917ddb83f
rhyo <saimrey.rhyolito@ti.ukdw.ac.id>
2021-11-12 11:23:11 +0700

    menambahkan jawaban nomor 3

---
+++ 3_E_71210804.py
@@ -0,0 +1,11 @@
+#input
+db1 = (input("masukan daftar belanjaan 1 : "))
+db2 = (input("masukan daftar belanjaan 2 : "))
+tdb1 = (input("tambahkan daftar belanjaan 1 : "))
+tdb2 = (input("tambahkan daftar belanjaan 2 : "))
+#proses
+r = (db1 + "," + tdb1)
+s = (db2 + "," + tdb2)
+#cetak di layar
+print  ("Masukan daftar belanja 1", r)
+print  ("Masukan daftar belanja 2", s)
