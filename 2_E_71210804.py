47539ce4ac68a4fb1be6906973dc417bf5251bd9
rhyo <saimrey.rhyolito@ti.ukdw.ac.id>
2021-11-12 11:22:06 +0700

    menambahkan jawaban nomor 2

---
+++ 2_E_71210804.py
@@ -0,0 +1,17 @@
+"Test case 1"
+#input
+depan, belakang, = list(input("Nama : ").split())
+tempat, tanggal, bulan, tahun = list(input("Tempat tanggal lahir : ").split())
+
+#cetak di layar
+print("Haloo!", belakang,",", depan)
+print("Anda lahir di", tempat, "pada", tanggal, bulan, tahun)
+
+"Test case 2"
+#input
+depan, tengah, belakang, = list(input("Nama : ").split())
+tempat, tanggal, bulan, tahun = list(input("Tempat tanggal lahir : ").split())
+
+#cetak di layar
+print("Haloo!", belakang,",", depan, tengah)
+print("Anda lahir di", tempat, "pada", tanggal, bulan, tahun)
