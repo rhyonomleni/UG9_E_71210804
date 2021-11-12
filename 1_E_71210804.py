06b7fba4a5f41ec3e7d14725e23cfd0e302f4ad7
rhyo <saimrey.rhyolito@ti.ukdw.ac.id>
2021-11-12 11:19:44 +0700

    menambahkan jawaban nomor 1

---
+++ 1_E_71210804.py
@@ -0,0 +1,10 @@
+#input
+aa = int (input("masukan alas atas : "))
+ab = int (input("masukan alas bawah : "))
+tinggi = int (input("masukan tinggi : "))
+
+#proses
+luas = 0.5 * (aa + ab) * tinggi
+
+#cetak layar
+print ("luas trapesium adalah : ", luas)
